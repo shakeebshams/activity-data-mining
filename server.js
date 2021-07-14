f5752187e309caba1f2c8ac194a0e0165facc4a2
shakeebshams <shakeebshams@gmail.com>
2021-07-14 11:02:03 -0400

    complete core of code

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ let db
 
 async function main() {
     //using for loop intead of for each as it would be processed async by default
-    MongoClient.connect(DBurl, { useNewUrlParser: true }, (err, client) => {
+    MongoClient.connect(DBurl, { useNewUrlParser: true }, async (err, client) => {
         if (err) return console.log(err);
         // Storing a reference to the database so you can use it later
         db = client.db(dbName);
@@ -27,20 +27,21 @@ async function main() {
         let collection = db.collection("citylist");
         let cursor = collection.find({});
         //console.log(cursor);
-        let docArray = cursor.toArray();
-        let arr = collection.find().snapshot();
-        console.log(arr.length)
-        /*
-        collection.find().snapshot().forEach(async function(doc) {
-            console.log("now precessing: " + doc.id);
+        //let docArray = cursor.toArray();
+        //let arr = collection.find().snapshot();
+        //console.log(arr.length)
+        
+        await collection.find().snapshot().forEach(async function(doc) {
             try {
-                await activity_request(doc.id);
+                if (doc.id) {
+                    console.log("city_id: " + doc.id);
+                    await activity_request(doc.id);
+                }
             } catch (err) {
                 console.error(error);
             }
         });
-        */
-        activity_request('60898');
+        console.log("DONE DONE DONE DONE DONE DONE DONE DONE DONE DONE DONE DONE DONE")
     });
 }
 
@@ -51,13 +52,15 @@ async function activity_request(location_id) {
         while (next) {
             await axios.get(start_url, { 'headers': {'X-TripAdvisor-API-Key': process.env.API_KEY} })
                 .then((response) => {
-                    console.log(response.data.data.length);
-                    process_data(response.data.data);
-                    console.log(response.data.paging.next);
-                    if (response.data.paging.next === null) {
+                    if (response.data && response.data.paging) {
+                        process_data(response.data.data);
+                        if (response.data.paging.next === null) {
+                            next = false;
+                        }
+                        start_url = response.data.paging.next;
+                    } else {
                         next = false;
                     }
-                    start_url = response.data.paging.next;
                 })
                 .catch((error) => {
                     console.log(error);
@@ -74,28 +77,49 @@ async function activity_request(location_id) {
 async function process_data(data) {
     for (let i = 0; i < data.length; i++) {
         let activity = data[i];
-        let activity_data = {
-            name: activity.name,
-            latitude: activity.latitude,
-            longitude: activity.longitude,
-            type: activity.subcategory_type_label,
-            tripadvisor_ranking: activity.ranking,
-            price_range: activity.price,
-            stars: activity.hotel_class,
-            description: activity.description,
-            website: activity.website,
-            phone: activity.phone,
-            email: activity.email,
-            address: activity.address,
-            city: activity.address_obj.city,
-            state: activity.address_obj.state,
+        let isGolf = false;
+        let types = activity.subtype;
+        if (types) {
+            types.forEach(t => {
+                if (t.key === '60') {
+                    isGolf = true;
+                }
+            });
+            let activity_data = {
+                name: activity.name,
+                rating: activity.rating,
+                description: activity.description,
+                num_reviews: activity.num_reviews,
+                website: activity.website,
+                phone: activity.phone,
+                email: activity.email,
+                address: activity.address,
+                city: activity.address_obj.city,
+                state: activity.address_obj.state,
+                country: activity.address_obj.country,
+                latitude: activity.latitude,
+                longitude: activity.longitude,
+                tripadvisor_id: activity.location_id
+            }
+            if (isGolf && activity_data.country === 'United States') {
+                let collection2 = db.collection("golfUS3");
+                await collection2.insertOne(activity_data, function(err, res) {
+                    if (err) {
+                        console.log(err);
+                    };
+                    console.log("ADDED " + activity_data.city);
+                })
+            }
+            /*
+            let collection2 = db.collection("activitylist");
+            await collection2.insertOne(activity_data, function(err, res) {
+                if (err) {
+                    console.log(err);
+                };
+            })
+            */
         }
-        let collection = db.collection("hotels-again");
-        await collection.insertOne(hotel_data, function(err, res) {
-            if (err) {
-                console.log(err);
-            };
-        })
+        
     }
 }
 
